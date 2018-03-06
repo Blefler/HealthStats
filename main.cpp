8647332148cd3eaab8e4dd360a0e9361704d6954
kbuffardi <kbuffardi@ecc-jaguar.csuchico.edu>
2018-03-05 16:03:15 -0800

    Adds a couple BPM and shows the list

--- main.cpp
+++ main.cpp
@@ -10,5 +10,17 @@ int main()
     vector <int> steps;
     vector <int> calories;
 
+    //bpm.resize(10);
+    bpm.push_back(90);
+    bpm.push_back(100);
+    bpm.push_back(70);
+   
+    cout<<"BPM is currently size "<<bpm.size()<<endl;
+    cout<<"The last item in BPM is... "<<bpm[ bpm.size() -1 ]<<endl;
+    cout<<"The items in BPM are..."<<endl;
+    for(int i = 0; i < bpm.size(); i++)
+    {
+        cout<<bpm[i]<<endl;
+    }
     return 0;
 }
